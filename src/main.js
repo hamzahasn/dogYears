50c0145bcb7f80c8e58d8a40c9016b7c8e5b60d8
hamzahasn <hamza.hasnain@gmail.com>
2021-01-02 03:07:38 +0000

    js file complete

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// Variable containing my age.
+const myAge = 28;
+// Variable that will change.
+let earlyYears = 2;
+earlyYears *= 10.5;
+// Later Years variable
+let laterYears = myAge - 2;
+// Calculates number of dog years
+laterYears *= 4;
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+console.log(earlyYears);
+console.log(laterYears);
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
-};
+// Age in dog years.
+let myAgeInDogYears = earlyYears + laterYears;
+
+let myName = "Hamza".toLowerCase();
+console.log(
+  `My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`
+);
